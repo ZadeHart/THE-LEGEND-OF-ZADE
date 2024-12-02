d3384f71e21cfa79a32c0704cd1889dad023f11c
Zade Hart <zadehartpro@gmail.com>
2024-12-02 00:23:59 -0700

    added Netlify CDN

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ export default function Footer () {
     return (
         <footer className="footer">
             <a href="https://github.com/ZadeHart">
-            <img src="../assets/8bit_github_icon.png" alt="" />
+            <img src="/.netlify/images?url=/assets/8bit_github_icon.png" alt="" />
             </a>
             <a href="https://www.linkedin.com/in/zachary-de-hart-29a1a481/">
-            <img src="../assets/8bit_linkedin_icon.png" alt="" />
+            <img src="/.netlify/images?url=/assets/8bit_linkedin_icon.png" alt="" />
             </a>
             <a href="https://app.slack.com/client/T07AQPA57QT/C07AAAZGXGF">
-            <img src="../assets/8bit_slack_icon.png" alt="" />
+            <img src="/.netlify/images?url=/assets/8bit_slack_icon.png" alt="" />
             </a>
         </footer>
     );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,30 +2,30 @@ export default function Portfolio() {
   return (
     <div>
       <h2>Portfolio</h2>
-      <img src='assets/chest.gif'/>
+      <img src='/.netlify/images?url=/assets/chest.gif'/>
         <section className="card">
         <p>Weather Tracker</p>
         <a href="https://do-you-think-it-will-rain.onrender.com/">
-          <img src="../assets/weather_api.png" alt="" />
+          <img src="/.netlify/images?url=/assets/weather_api.png" alt="" />
         </a>
         </section>
 
           <div className="main">
             <section className="card">
               <p>Webpage</p>
-              <img src="../assets/challenge_01.png" alt="" />
+              <img src="/.netlify/images?url=/assets/challenge_01.png" alt="" />
             </section>
             <section className="card">
               <p>Portfolio</p>
-              <img src="../assets/challenge_02.png" alt="" />
+              <img src="/.netlify/images?url=/assets/challenge_02.png" alt="" />
             </section>
             <section className="card">
               <p>Payroll Tracker</p>
-              <img src="../assets/challenge_03.png" alt="" />
+              <img src="/.netlify/images?url=/assets/challenge_03.png" alt="" />
             </section>
             <section className="card">
               <p>Blog</p>
-              <img src="../assets/challenge_04.png" alt="" />
+              <img src="/.netlify/images?url=/assets/challenge_04.png" alt="" />
             </section>
           </div>
     </div>
