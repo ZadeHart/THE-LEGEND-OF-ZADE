92f87889fb44bc2349d0d8545655f5cb7ee85e19
Zade Hart <zadehartpro@gmail.com>
2024-12-02 00:28:17 -0700

    undid Netlify CDN

--- src/components/Contact_Me.jsx
+++ src/components/Contact_Me.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
 
     <div>
       <h2>Contact Me</h2>
-      <img src='/.netlify/images?url=/assets/hold_the_phone.gif'/>
+      <img src='../assets/hold_the_phone.gif'/>
 
           <form id="form">
             <label>Name:
